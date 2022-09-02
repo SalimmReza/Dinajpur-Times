67566ebdbee74aef885666ce9f09738ed77573a2
SalimmReza <salimnuaa@gmail.com>
2022-09-03 01:19:08 +0600

    modal js

--- index.js
+++ index.js
@@ -67,10 +67,10 @@ const displayCatagoryDetais = cId => {
                     <div class="d-flex justify-content-between w-50">
                     <img src="${thumbnail_url}"  class="rounded-circle" style="height:50px; width:50px; border-radius: 50%;" alt="...">
                         <p>${total_view}</p>
-                        <button onclick="MoreDetails(${_id})" type="button" class="btn btn-primary"
-            data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-               Details
-            </button>
+                        <button onclick='loadMoreDetails("${_id}")' type="button" class="btn btn-primary"
+                        data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                            Launch static backdrop modal
+                        </button>
                     </div>
                 </div>
             </div>
@@ -83,9 +83,45 @@ const displayCatagoryDetais = cId => {
 }
 
 
+
 // ----------modal details-------------
 
+const loadMoreDetails = (_id) => {
+    const url = `https://openapi.programming-hero.com/api/news/${_id}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => moreDetails(data.data))
+    // .catch(error => console.log(error))
+}
 
+
+
+const moreDetails = modal => {
+    console.log(modal);
+    const detailsCard = document.getElementById('modal');
+    const createDiv = document.createElement('div');
+    createDiv.innerHTML = `
+        <div class="card mb-3"">
+        <div class="row g-0">
+            <div class="col-md-4">
+                <img src="" class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <p class="card-text">sdsdsd</p>
+                    <div class="d-flex justify-content-between w-50">
+                        <p>dsds</p>
+                     
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+         `;
+
+    detailsCard.appendChild(createDiv);
+
+}
 
 
 
