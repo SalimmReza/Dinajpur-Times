46daf6ab0a1a83df2b784d2cccc0ca61f0323ac3
SalimmReza <salimnuaa@gmail.com>
2022-09-03 18:52:57 +0600

    done

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const categoryName = async () => {
 
     const categoryCard = document.getElementById('catagories-card');
     data.forEach(cName => {
+
         const { category_name } = cName;
         // console.log(cName.category_id)
         // console.log(cName.category_name);
@@ -27,9 +28,10 @@ const categoryName = async () => {
         createDiv.innerHTML = `
         <div class=""  >
          <p class="inline px-3 py-md-2" onclick ='LoadDetails("${cName.category_id}")'>${category_name}</p>
-          
+         
             </div >
     `;
+
         categoryCard.appendChild(createDiv);
     })
 
@@ -38,6 +40,7 @@ const categoryName = async () => {
 // ----------details-----
 
 const LoadDetails = (category_id) => {
+
     // const actName = categoryName();
     const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`
     fetch(url)
@@ -47,20 +50,29 @@ const LoadDetails = (category_id) => {
 }
 
 const displayCatagoryDetais = (cId) => {
-
     // const actName = categoryName(category_name);
     toggle(true);
     // console.log(cId);
     // console.log(actName);
     // -------------length of array-----------
     // console.log(cId.length);
+
+    //display no phones found
+    const h2 = document.getElementById('no-phones-found');
+    if (cId.length === 0) {
+        h2.classList.remove('d-none');
+    }
+    else {
+        h2.classList.add('d-none');
+    }
+
     const countArray = document.getElementById('count-items');
     countArray.innerHTML = ``;
     const createDiv = document.createElement('div');
     createDiv.innerHTML = `
      <div class="card">
                 <div class="card-body">
-                    <h5 class="card-title">${cId.length} items found</h5>
+                    <h5 class="card-title">${cId.length} Items Found</h5>
                 </div>
             </div>
     `;
@@ -75,7 +87,7 @@ const displayCatagoryDetais = (cId) => {
     cId.sort((a, b) => b.total_view - a.total_view);
 
     cId.forEach((cDetails) => {
-        console.log(`${cDetails.total_view}`);
+        // console.log(`${cDetails.total_view}`);
     });
     // -------------------------sort--end
     cId.forEach(cDetails => {
@@ -90,24 +102,24 @@ const displayCatagoryDetais = (cId) => {
         <div class="card mb-3"">
         <div class="row g-0">
             <div class="col-md-4">
-                <img src="${image_url}" class="img-fluid rounded-start" alt="...">
+                <img src="${image_url}" class="img-fluid h-100 rounded-start" alt="...">
             </div>
             <div class="col-md-8">
                 <div class="card-body mt-4">
-                    <h5 class="card-title">${title}</h5>
-                    <p class="card-text">${details.slice(0, 200)}</p>
+                    <h5 class="card-title">${title ? title : "no title"}</h5>
+                    <p class="card-text">${details.length > 300 ? details.slice(0, 300) + "..." : details}</p>
                     <div class="d-flex justify-content-between infos" >
                     <div class="d-flex align-items-center">
-                    <img src="${thumbnail_url}"  class="rounded-circle m-2" style="height:50px; width:50px; border-radius: 50%;" alt="...">
+                    <img src="${img}"  class="rounded-circle m-2" style="height:50px; width:50px; border-radius: 50%;" alt="...">
                     <div> 
-                    <h5 class =" m-0 text-start" >${name}</h5>
+                    <h5 class =" m-0 text-start" >${name ? name : "no name"}</h5>
                     <h5 class ="text-start m-0" style ="font-size: 12px;" >${published_date ? published_date.slice(0, 10) : "no date"}</h5>
                     </div>
                     </div>
                    
                     <div class="d-flex align-items-center">
                     <i class="fa-solid fa-eye m-2"></i>
-                    <p class ="text-center" style="margin-top: 15px;">${total_view}</p>
+                    <p class ="text-center" style="margin-top: 15px;">${total_view ? total_view : "no views"}</p>
                 </div>
                 <div class="mt-4 mx-3">
                 <i onclick='loadMoreDetails("${_id}")' class="fa-solid fa-arrow-right class="btn btn-primary"
@@ -145,33 +157,28 @@ const loadMoreDetails = (_id) => {
     const url = `https://openapi.programming-hero.com/api/news/${_id}`
     fetch(url)
         .then(res => res.json())
-        .then(data => moreDetails(data.data))
-    // .catch(error => console.log(error))
+        .then(data => moreDetails(data.data[0]))
+        .catch(error => console.log(error))
 }
 
 
 
 const moreDetails = modal => {
     console.log(modal);
+
+    // const { name, published_date } = author;
     const detailsCard = document.getElementById('modal');
+    detailsCard.innerHTML = ``;
     const createDiv = document.createElement('div');
     createDiv.innerHTML = `
-        <div class="card mb-3"">
-        <div class="row g-0">
-            <div class="col-md-4">
-                <img src="" class="img-fluid rounded-start" alt="...">
-            </div>
-            <div class="col-md-8">
-                <div class="card-body">
-                    <p class="card-text">sdsdsd</p>
-                    <div class="d-flex justify-content-between w-50">
-                        <p>dsds</p>
-                     
-                    </div>
+    <img src="${modal.image_url}"class="w-100 h-25" alt="">
+    <p>${modal.details}</p>
+    <h5 class =" m-0 text-start" >${modal.author.name ? modal.author.name : "no name"}</h5>
+    <p class="fw-bold">Date: ${modal.author.published_date ? modal.author.published_date : "no date"}</p>
+    <div class="d-flex align-items-center">
+                    <i class="fa-solid fa-eye m-2"></i>
+                    <p class ="text-center" style="margin-top: 15px;">${modal.total_view ? modal.total_view : "no views"}</p>
                 </div>
-            </div>
-        </div>
-    </div>
          `;
 
     detailsCard.appendChild(createDiv);
@@ -183,4 +190,4 @@ const moreDetails = modal => {
 
 // loadNews();
 categoryName();
-LoadDetails('03');
+LoadDetails('08');
